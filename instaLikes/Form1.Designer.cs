cf5247c589335addeb5b07498e5fa7f5d3fa6758
Alex Vahtin <iqlex@mail.ru>
2017-10-10 22:35:02 +0300

    Add project

---
+++ instaLikes/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace лойсы_инст
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 82);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Добавить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(10, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(249, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(366, 198);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(218, 214);
+            this.textBox2.TabIndex = 5;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 50;
+            this.hScrollBar1.Location = new System.Drawing.Point(10, 62);
+            this.hScrollBar1.Maximum = 5000;
+            this.hScrollBar1.Minimum = 50;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(249, 17);
+            this.hScrollBar1.SmallChange = 50;
+            this.hScrollBar1.TabIndex = 6;
+            this.hScrollBar1.Value = 1000;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(10, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(57, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(10, 198);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(350, 214);
+            this.checkedListBox1.TabIndex = 8;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            this.checkedListBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.checkedListBox1_KeyDown);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(73, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(193, 19);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "накрутка на несколько фото";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(265, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 180);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "INLIKE";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(596, 421);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "INLIKE";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        public System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ instaLikes/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net.NetworkInformation;
+
+namespace лойсы_инст
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        Encoding enc = Encoding.UTF8;
+        int kolvo,count;
+        public static string str;
+        Thread[] thr;
+        string[] urls;       
+        public Form1()
+        {
+            InitializeComponent();
+            loadParam();
+            pictureBox1.Image = new Bitmap(Image.FromFile("ins.png"), pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void loadParam()
+        {
+            FileStream file = new FileStream(@"text.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader read = new StreamReader(@"text.txt", enc);
+            try
+            {
+                while (!read.EndOfStream)
+                {
+                    string Item1 = read.ReadLine();
+                    if (Item1.Length > 2)
+                    {
+                        comboBox1.Items.Add(Item1);
+                        checkedListBox1.Items.Add(Item1);
+                    }
+                }
+            }
+            catch { MessageBox.Show("Ошибочка вышла.\nСоре"); }
+            read.Close();
+            file.Close();
+        }
+        private string FindID(string address)
+        {
+            HttpWebRequest req;
+            HttpWebResponse resp;
+            StreamReader sr;
+            string content;
+            req = (HttpWebRequest)WebRequest.Create(address);
+            resp = (HttpWebResponse)req.GetResponse();
+            sr = new StreamReader(resp.GetResponseStream(), Encoding.GetEncoding("windows-1251"));
+            content = sr.ReadToEnd();
+            sr.Close();
+            Regex myReg = new Regex("\"" + @"\d+" + "\"");
+            MatchCollection matches = myReg.Matches(content);           
+            string first = matches[3].Value;
+            first=first.Remove(0, 1);
+            first=first.Remove(first.Length-1, 1);
+            string second = matches[4].Value;
+            second=second.Remove(0, 1);
+            second=second.Remove(second.Length-1, 1);
+            string result = string.Format("http://75.102.21.228/add?id={0}_{1}", first, second);
+            return result;        
+        }
+        private void FindImage(object address)
+        {
+            try {
+                pictureBox1.Image = new Bitmap(Image.FromFile("load.png"), pictureBox1.Width, pictureBox1.Height);
+                HttpWebRequest req;
+                HttpWebResponse resp;
+                StreamReader sr;
+                string content;
+                req = (HttpWebRequest)WebRequest.Create(address as string);
+                resp = (HttpWebResponse)req.GetResponse();
+                sr = new StreamReader(resp.GetResponseStream(), Encoding.GetEncoding("windows-1251"));
+                content = sr.ReadToEnd();
+                sr.Close();
+                Regex regn = new Regex("\"og:image\" content=\"" + @"\S+" + "jpg");
+                MatchCollection matches2 = regn.Matches(content);
+                string res = matches2[0].Value;
+                res = res.Remove(0, 20);
+                var request = WebRequest.Create(res);
+                using (var response = request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                {
+                    Image img = Bitmap.FromStream(stream);
+                    Bitmap bm = new Bitmap(img, pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = bm;
+                }
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (checkBox1.Checked)
+            {
+                case true:
+                    if (kolvo == 0) kolvo = 20000;
+                    thr = new Thread[kolvo];                    
+                    urls = new string[checkedListBox1.CheckedItems.Count];
+                    int cont=0;                    
+                    foreach (var chck in checkedListBox1.CheckedItems)
+                    {
+                        urls[cont] = FindID(chck.ToString());
+                        cont++;
+                    }
+                    timer2.Enabled = true;
+                    timer2.Start();
+                    timer2.Interval = hScrollBar1.Value;
+                    break;
+                case false:                   
+                    str = comboBox1.SelectedItem.ToString();
+                    if (kolvo == 0) kolvo = 20000;
+                    thr = new Thread[kolvo];
+                    timer1.Enabled = true;
+                    timer1.Start();
+                    timer1.Interval = hScrollBar1.Value;
+                    break;
+            }            
+        }
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+                notifyIcon1.Visible = false;
+            }
+        }
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer1.Stop();
+            timer2.Enabled = false;
+            timer2.Stop();
+            MessageBox.Show(count.ToString());
+        }       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            if (i == 15000)
+            {
+                Array.Clear(thr, 0, 20000);
+                thr = new Thread[20000];
+                i = 0;
+            }
+            i++;
+            thr[i] = new Thread(new ParameterizedThreadStart(xer));
+            thr[i].Name = i.ToString();
+            thr[i].Start(str);           
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ButtonVoidAdd();
+        }     
+        private void ButtonVoidAdd()
+        {
+            FileStream file1 = new FileStream(@"text.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter wrt = new StreamWriter(file1, enc);
+            string AddAddress = textBox1.Text;
+            wrt.WriteLine(AddAddress);
+            wrt.Close();
+            comboBox1.Items.Add(AddAddress);
+            checkedListBox1.Items.Add(AddAddress);
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            textBox3.Text = hScrollBar1.Value.ToString();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                hScrollBar1.Value = Int32.Parse(textBox3.Text);
+            }
+            catch { }
+        }
+
+        delegate void Del(string text);
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            foreach(string rqUrl in urls)
+            {
+                count++;
+                if (i == 15000)
+                {
+                    Array.Clear(thr, 0, 20000);
+                    thr = new Thread[20000];
+                    i = 0;
+                }
+                i++;
+                thr[i] = new Thread(new ParameterizedThreadStart(xer));
+                thr[i].Name = i.ToString();
+                thr[i].Start(rqUrl);
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete) { textBox1.Clear(); }
+            if(e.KeyCode == Keys.Enter)
+            {
+                Regex reg = new Regex(@"\S+");
+                string text = reg.Match(textBox1.Text).ToString();
+                textBox1.Text = text;
+                ButtonVoidAdd(); textBox1.Clear();
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Thread proc = new Thread(new ParameterizedThreadStart(FindImage));
+                proc.Start(checkedListBox1.SelectedItem.ToString());
+            }
+            catch { }
+        }
+
+        private void checkedListBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Delete)
+            {
+                int index = checkedListBox1.SelectedIndex;
+                FileStream file1 = new FileStream(@"text.txt", FileMode.Open, FileAccess.Read);
+                StreamReader read = new StreamReader(file1, enc);
+                string text = read.ReadToEnd();
+                Regex reg = new Regex(checkedListBox1.SelectedItem.ToString());
+                text = reg.Replace(text,"");
+                read.Close();
+                file1 = new FileStream(@"text.txt", FileMode.Truncate, FileAccess.Write);
+                StreamWriter wrt = new StreamWriter(file1, enc);
+                wrt.WriteLine(text);
+                wrt.Close();
+                checkedListBox1.Items.Remove(checkedListBox1.SelectedItem);
+                comboBox1.Items.RemoveAt(index);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Thread th = new Thread(new ParameterizedThreadStart(FindImage));
+            th.Start(comboBox1.SelectedItem.ToString());           
+        }
+    
+        private void xer(object adr)
+        {
+            string adres = (string)adr;
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(adres);
+            req.GetResponseAsync();
+            Thread.Sleep(10000);
+            req.Abort();
+            object locker1 = new object();
+            lock (locker1)
+            {
+                textBox1.Invoke(new Del((s) => textBox2.Text = s), Thread.CurrentThread.Name);
+            }
+        }
+    }
+}
